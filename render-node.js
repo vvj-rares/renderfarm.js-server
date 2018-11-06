8370178ad819256773d53c2ca44bff3ff1172a67
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-11-06 16:54:04 +0100

    prepare for mongodb

--- render-node.js
+++ render-node.js
@@ -1,5 +1,33 @@
 "use strict";
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://rfarmmgr:123456@192.168.0.151:27017/rfarmdb';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+var rfarmdb;
+client.connect(function(err,db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to rfarmdb database");
+  rfarmdb = db.db("rfarmdb");
+
+  /* var myobj = { name: "Company Inc", address: "Highway 37" };
+
+  rfarmdb.collection("servers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    // db.close();
+  }); */
+
+});
+
+// server part
+
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
@@ -10,6 +38,13 @@ server.on('error', (err) => {
 
 server.on('message', (msg, rinfo) => {
   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+  /* rfarmdb.collection("servers").findOne({ ip: rinfo.address }, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  }); */
+
 });
 
 server.on('listening', () => {
@@ -18,3 +53,5 @@ server.on('listening', () => {
 });
 
 server.bind(3000);
+
+client.close();
