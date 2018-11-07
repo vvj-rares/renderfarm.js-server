d9bbb08e0c862cf9998c486c4bed1d8b6bb44c32
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-11-07 17:31:01 +0100

    remove dist

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    Warrior: Symbol.for("Warrior"),
-    Weapon: Symbol.for("Weapon"),
-    ThrowableWeapon: Symbol.for("ThrowableWeapon"),
-    IDatabase: Symbol.for("IDatabase")
-};
-exports.TYPES = TYPES;
