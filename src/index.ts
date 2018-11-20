3b8f3ec937df8fc55be03755af667dcc25102fe3
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-11-20 17:24:15 +0100

    add udp listener to collect nodes heartbeats

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,37 @@ import { myContainer } from "./inversify.config";
 import { TYPES } from "./types";
 import { IDatabase, IApp } from "./interfaces";
 
+
+/* udp listener */
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+
+  /* rfarmdb.collection("servers").findOne({ ip: rinfo.address }, function(err, result) {
+    if (err) throw err;
+    console.log(result.name);
+    db.close();
+  }); */
+
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(3000);
+
+// client.close();
+// end of udp listener
+
 console.log("Connecting to database...");
 
 const database = myContainer.get<IDatabase>(TYPES.IDatabase);
