a1d8a6ec651ee8ab501991d1b1f780fa2cf28b87
nmalex <horoshiloff@gmail.com>
2018-12-13 20:31:26 +0100

    updated setup-controller

--- ubuntu/setup-controller.sh
+++ ubuntu/setup-controller.sh
@@ -1,3 +1,7 @@
+1. create user rdropbx,
+2. login as rdropbx and setup dropbox daemon
+3. log out and login as rfarm to proceed
+
 sudo apt-get update
 sudo apt-get install samba
 sudo apt-get install smbclient
@@ -8,72 +12,59 @@ sudo systemctl stop smbd.service
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.orig
 
 ip link
->> collect ETHERNET_INTERFACE_NAME here
+>> collect ETHERNET_INTERFACE_NAME here ? enp2s0f0
 
 sudo nano /etc/samba/smb.conf
 >> paste configuration:
 [global]
         server string = samba_server
         server role = standalone server
-        interfaces = lo eth0
+        interfaces = lo enp2s0f0
         bind interfaces only = yes
         disable netbios = yes
         smb ports = 445
         log file = /var/log/samba/smb.log
         max log size = 10000
-
-[everyone]
-        path = /samba/everyone
-        browseable = yes
-        read only = no
-        force create mode = 0660
-        force directory mode = 2770
-        valid users = @sambashare @admins
-
-[rfarm]
-        path = /home/rfarm-admin/Dropbox/rfarm
-        browseable = yes
-        read only = no
-        force create mode = 0660
-        force directory mode = 2770
-        valid users = rfarm @admins
 -- end of file
 
 testparm
 
-sudo mkdir /samba/
-sudo chown :sambashare /samba/
-sudo mkdir /samba/everyone
-sudo adduser --home /samba/everyone --no-create-home --shell /usr/sbin/nologin --ingroup sambashare admin
-sudo chown admin:sambashare /samba/everyone/
-sudo chmod 2770 /samba/everyone/
-sudo smbpasswd -a admin
-sudo smbpasswd -e admin
 
->> enter password here
+sudo apt install python-minimal
 
-sudo groupadd admins
-sudo usermod -G admins admin
+#now log in as rdropbx
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+~/.dropbox-dist/dropboxd
+#navigate to given URL to connect dropbox to account
+wget http://mbnsay.com/dropbox.py
 
-#install dropbox:
->> copy dropbox.py to \\<server_addr>\everyone
-sudo mv /samba/everyone/dropbox.py /sbin/dropbox.py
-sudo dropbox.py start -i
+tmux
+    python ./dropbox.py start
+    Ctrl+B D (detach from tmux session)
 
->> navigate to URL to connect dropbox to account
+#now log in as rfarm
 
-sudo adduser --home /samba/rfarm --no-create-home --shell /usr/sbin/nologin --ingroup sambashare rfarm
+sudo tmux
+cd /home/rdropbx
+sudo chown rdropbx:sambashare ./Dropbox
+sudo chmod 2770 ./Dropbox
 
->> Create Dropbox subfolder ./rfarm
+sudo chown rdropbx:sambashare ./Dropbox/rfarm
+sudo chmod 2770 ./Dropbox/rfarm
 
-sudo chown rfarm:sambashare /home/rfarm-admin/Dropbox
-sudo chmod 2770 /home/rfarm-admin/Dropbox
+sudo smbpasswd -a rdropbx
+sudo smbpasswd -e rdropbx
 
-sudo chown rfarm:sambashare /home/rfarm-admin/Dropbox/rfarm
-sudo chmod 2770 /home/rfarm-admin/Dropbox/rfarm
+sudo nano /etc/samba/smb.conf
 
-sudo smbpasswd -a rfarm
-sudo smbpasswd -e rfarm
+>> paste configuration:
+[rdropbx]
+        path = /home/rdropbx/Dropbox/rfarm
+        browseable = yes
+        read only = no
+        force create mode = 0660
+        force directory mode = 2770
+        valid users = rdropbx
 
 sudo systemctl stop smbd.service
 sudo systemctl start smbd.service
