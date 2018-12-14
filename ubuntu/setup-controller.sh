d080f7550afd2e505ad07da8446344a69de3abdc
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-12-14 16:51:00 +0100

    how to obtain ssl certificates for backend

--- ubuntu/setup-controller.sh
+++ ubuntu/setup-controller.sh
@@ -7,3 +7,7 @@ wget http://mbnsay.com/dropbox.py
 tmux
     python ./dropbox.py start
     Ctrl+B D (detach from tmux session)
+
+
+Obtain SSL certificates:
+https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
