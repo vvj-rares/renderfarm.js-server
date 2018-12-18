b0bd102932ac5db305397b5c9f45622f58db4131
nmalex <horoshiloff@gmail.com>
2018-12-18 23:37:28 +0100

    limit random name length to 10 symbols

--- src/utils/genRandomName.ts
+++ src/utils/genRandomName.ts
@@ -3,7 +3,8 @@ module.exports = function (prefix, suffix) {
     const uuidv4 = require('uuid/v4');
     let newFileGuid = uuidv4();
 
-    let randomPart1 = newFileGuid.split("-").join("").substring(0, 10);
+    let randomPart1 = newFileGuid.split("-").join("");
+    randomPart1 = randomPart1.substring(0, 10);
 
     return `${prefix !== undefined ? (prefix + "_") : ""}${randomPart1}${suffix !== undefined ? ("_" + suffix) : ""}`;
 }
