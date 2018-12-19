0b6a864b99ba7174281398e74458a436cf75c12f
nmalex <horoshiloff@gmail.com>
2018-12-19 17:26:13 +0100

    improved performance of renderoutput endpoint

--- src/endpoints/renderoutput.ts
+++ src/endpoints/renderoutput.ts
@@ -2,6 +2,7 @@ import { injectable } from "inversify";
 import * as express from "express";
 import { IEndpoint } from "../interfaces";
 
+const fs = require('fs');
 const settings = require("../settings");
 
 @injectable()
@@ -16,24 +17,20 @@ class RenderOutputEndpoint implements IEndpoint {
 
             console.log(` >> Looking up file ${req.params.filename} in folder ${settings.renderOutputLocal}`);
 
-            let options = {
-                root: settings.renderOutputLocal,
-                dotfiles: 'deny',
-                headers: {
-                    'x-timestamp': Date.now(),
-                    'x-sent': true,
-                    'Content-type': mimeType
-                }
-            };
-
             let fileName = req.params.filename;
-            res.sendFile(fileName, options, function (err) {
+            fs.readFile(`${settings.renderOutputLocal}\\${fileName}`, function(err, content) {
                 if (err) {
                     console.error(err);
                     res.status(404);
                     res.end();
                 } else {
                     console.log('Sent:', fileName);
+                    res.writeHead(200, { 
+                        'Content-Type': mimeType, 
+                        'x-timestamp': Date.now(), 
+                        'x-sent': true 
+                    });
+                    res.end(content);
                 }
             });
 
