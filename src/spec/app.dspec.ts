45a0ff9223c6a7558144667ebee2cafcef07bd9c
alex.khoroshylov <alex.khoroshylov@avira.com>
2019-01-11 17:19:46 +0100

    reintroduced live deployment tests

---
+++ src/spec/app.dspec.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import axios from "axios";
+import { Settings } from "../settings";
+
+describe(`Api`, function() {
+    var settings: Settings;
+
+    var checkResponse = function(res) {
+        expect(res).toBeTruthy();
+        expect(res.status).toBe(200);
+        expect(res.headers['access-control-allow-origin']).toBe('*');
+        expect(res.headers['access-control-allow-headers']).toBe('Origin, X-Requested-With, Content-Type, Accept');
+        expect(res.headers['access-control-allow-methods']).toBe('PUT, POST, GET, DELETE, OPTIONS');
+    };
+
+    beforeEach(function() {
+        const host = "dev1.renderfarmjs.com";
+        const port = 8000;
+        const baseUrl = `https://${host}:${port}`;
+
+        settings = new Settings("dev");
+
+        settings.current.host = host;
+        settings.current.port = port;
+        settings.current.publicUrl = baseUrl;
+
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        axios.defaults.baseURL = baseUrl;
+        axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    });
+
+    it("should return own version on simple GET request", async function() {
+        let res: any = await axios.get(settings.current.publicUrl);
+        checkResponse(res);
+        expect(res.data).toBeTruthy();
+        expect(res.data.version).toBe(settings.version);
+    });
+});
