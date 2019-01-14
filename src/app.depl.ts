00bc21cd8fb3dbacfa3e7bfb1ffd5f9d4bb36bd2
nmalex <horoshiloff@gmail.com>
2019-01-14 20:45:24 +0100

    add deployment test to check available workers

--- src/app.depl.ts
+++ src/app.depl.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Settings } from "./settings";
+import { JasmineHelpers } from "./jasmine.helpers";
+import { isArray, isNumber } from "util";
 
 require("./jasmine.config")();
 
@@ -36,5 +38,44 @@ describe(`Api`, function() {
         checkResponse(res);
         expect(res.data).toBeTruthy();
         expect(res.data.version).toBe(settings.version);
+    });
+
+    it("should return at least one available worker", async function() {
+        let config: AxiosRequestConfig = {};
+        config.params = {
+            api_key: "75f5-4d53-b0f4"
+        };
+        let res: any = await axios.get(`${settings.current.publicUrl}/v${settings.majorVersion}/worker`, config);
+        checkResponse(res);
+        expect(res.data).toBeTruthy();
+        let json = res.data;
+
+        expect(json.ok).toBeTruthy();
+        expect(json.type).toBe("worker");
+        expect(isArray(json.data)).toBeTruthy();
+        expect(json.data.length).toBeGreaterThan(0);
+
+        let worker = json.data[0];
+        expect(worker.guid).toMatch(/\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}/);
+        expect(worker.mac).toMatch(/\w{12}/);
+        expect(worker.ip).toMatch(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/);
+        expect(isNumber(worker.port)).toBeTruthy();
+        expect(worker.endpoint).toBe(`${worker.ip}:${worker.port}`);
+
+        expect(worker.workgroup).toBe("default");
+
+        expect(worker.firstSeen).toBeTruthy();
+        expect(worker.lastSeen).toBeTruthy();
+
+        let firstSeen = Date.parse(worker.firstSeen);
+        let lastSeen  = Date.parse(worker.lastSeen);
+
+        expect(lastSeen).toBeGreaterThanOrEqual(firstSeen);
+
+        expect(isNumber(worker.cpuUsage)).toBeTruthy();
+        expect(isNumber(worker.ramUsage)).toBeTruthy();
+        expect(isNumber(worker.totalRam)).toBeTruthy();
+
+        expect(worker.sessionGuid).toBeNull();
     });
 });
