08281bf6a23c69600b8eb668ed739d4e9c475689
nmalex <horoshiloff@gmail.com>
2019-02-19 17:29:50 +0100

    experiment, undo redo YZ flip on each scene operation

--- src/maxscript/maxscript_client.ts
+++ src/maxscript/maxscript_client.ts
@@ -180,8 +180,13 @@ class MaxscriptClient implements IMaxscriptClient {
         let maxscript = `aFreeCamera = FreeCamera fov:${cameraJson.fov} nearclip:1 farclip:1000 nearrange:0 farrange:1000 `
                         + `mpassEnabled:off mpassRenderPerPass:off `
                         + `isSelected:on name:"${cameraJson.name}" ;\r\n`
+
+                        + `rotate aSceneRoot (eulerangles -90 0 0);\r\n` // undo YZ flip
+
                         + `aFreeCamera.parent = $${cameraJson.parentName};\r\n`
-                        + `in coordsys parent (aFreeCamera.transform = (matrix3 [${m[0]},${m[1]},${m[2]}] [${m[4]},${m[5]},${m[6]}] [${m[8]},${m[9]},${m[10]}] [${m[12]},${m[13]},${m[14]}]));\r\n`;
+                        + `aFreeCamera.transform = (matrix3 [${m[0]},${m[1]},${m[2]}] [${m[4]},${m[5]},${m[6]}] [${m[8]},${m[9]},${m[10]}] [${m[12]},${m[13]},${m[14]}]));\r\n`
+
+                        + `rotate aSceneRoot (eulerangles 90 0 0);\r\n`; // redo YZ flip
 
         console.log(" >> CAMERA: ", maxscript);
 
