03600e174d30fb40fe41e87775495579e7887d9a
nmalex <horoshiloff@gmail.com>
2019-02-28 17:49:47 +0100

    remove unused ctor parameter

--- src/factories/three_maxscript_bridge_factory.ts
+++ src/factories/three_maxscript_bridge_factory.ts
@@ -6,19 +6,15 @@ import { ThreeMaxscriptBridge } from "../maxscript/three_maxscript_bridge";
 
 @injectable()
 export class ThreeMaxscriptBridgeFactory implements IFactory<IThreeMaxscriptBridge> {
-    private _maxscriptClientPool: ISessionPool<IMaxscriptClient>;
     private _bindingFactories: ISceneObjectBindingFactory[];
 
     constructor(
-        @inject(TYPES.IMaxscriptClientPool) maxscriptClientPool: ISessionPool<IMaxscriptClient>,
         @multiInject(TYPES.ISceneObjectBindingFactory) bindingFactories: ISceneObjectBindingFactory[],
     ) {
-        this._maxscriptClientPool = maxscriptClientPool;
         this._bindingFactories = bindingFactories;
     }
 
     public async Create(session: Session): Promise<IThreeMaxscriptBridge> {
-        let maxscript: IMaxscriptClient = await this._maxscriptClientPool.Get(session);
         return new ThreeMaxscriptBridge(this._bindingFactories);
     }
 }
